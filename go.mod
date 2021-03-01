d6cde2cec30941b311ce41815fc8ed3c76f2a91a
Adrian Nistor <nistor.adrian@agilefreaks.com>
2021-03-01 15:59:32 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module rosetta.example.com/m/v2
+
+go 1.13
+
+require (
+	github.com/LuisAcerv/goeth-api v0.0.0-20201011231850-afdc62b03d47 // indirect
+	github.com/btcsuite/btcd v0.21.0-beta // indirect
+	github.com/ethereum/go-ethereum v1.9.25 // indirect
+	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
+	golang.org/x/sys v0.0.0-20210301091718-77cc2087c03b // indirect
+)
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	Handlers "github.com/LuisAcerv/goeth-api/handler"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// Create a client instance to connect to our providr
+	client, err := ethclient.Dial("https://ropsten.infura.io/v3/<project_id>")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Create a mux router
+	r := mux.NewRouter()
+
+	// We will define a single endpoint
+	r.Handle("/api/v1/eth/{module}", Handlers.ClientHandler{client})
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
